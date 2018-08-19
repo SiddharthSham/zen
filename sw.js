f2f4fbded2a0fb2ef2887475f433c384329ddddb
Siddharth Sham <siddhsham@gmail.com>
2018-08-19 22:44:51 +0530

    Attempt at salvation

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const staticCacheName = "zen-static";
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-      navigator.serviceWorker.register('{{ site.baseurl }}/sw.js').then(function(registration) {
+      navigator.serviceWorker.register('/sw.js').then(function(registration) {
         // Registration was successful
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
       }, function(err) {
