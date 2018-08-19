5a36c6163b730ea958cb02467ceda54601d7cb03
Siddharth Sham <siddhsham@gmail.com>
2018-08-19 22:22:37 +0530

    Manual build SW

--- sw.js
+++ sw.js
@@ -2,57 +2,87 @@
 layout: null
 ---
 
-const staticCacheName = "gdad-s-river-static-v61";
+const APP_CACHE_NAME = 'zen-app';
+const staticCacheName = "zen-static";
 
-console.log("installing service worker");
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('{{ site.baseurl }}/sw.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
 
 const filesToCache = [
   "/",
   {% for page in site.html_pages %}
     '{{ page.url }}',
   {% endfor %}
-  {% for post in site.posts %}
+  {% for post in site.blog %}
     '{{ post.url }}',
   {% endfor %}
 
   // can be automated rather than manual entries
-  "/assets/images/bhavri-github-callbacks.png",
-  "/assets/images/bhavri-github-issues.png",
-  "/assets/images/jakethecake-svg-line-anime.png",
-  "/assets/images/svg-animated-mast-text-shapes-tweet.png",
-  "css/main.css",
-  "/about/",
+  "/assets/images/",
+  "/assets/js/",
+  "/assests/css/main.css",
+  "/assests/css/screen.css",
+  "/about.md",
   "/index.html"
 ];
 
-self.addEventListener("install", function(e){
-    self.skipWaiting();
+self.addEventListener('install',function(e){
     e.waitUntil(
-      caches.open(staticCacheName).then(function(cache){
-        return cache.addAll(filesToCache);
-      })
-    )
-  });
+        Promise.all([
+            caches.open(STATIC_CACHE_NAME),
+            caches.open(APP_CACHE_NAME),
+            self.skipWaiting()
+          ]).then(function(storage){
+            var static_cache = storage[0];
+            var app_cache = storage[1];
+            return Promise.all([
+              static_cache.addAll(CACHE_STATIC),
+              app_cache.addAll(CACHE_APP)]);
+        })
+    );
+});
 
-  self.addEventListener("activate", function(e){
+  self.addEventListener('activate', function(e) {
     e.waitUntil(
-      caches.keys().then(function(cacheNames){
-        return Promise.all(
-          cacheNames.filter(function(cacheName){
-            return cacheName.startsWith("gdad-s-river-static-")
-              && cacheName != staticCacheName;
-          }).map(function(cacheName){
-            return cache.delete(cacheName);
-          })
-        )
-      })
-    )
-  });
+        Promise.all([
+            self.clients.claim(),
+            caches.keys().then(function(cacheNames) {
+                return Promise.all(
+                    cacheNames.map(function(cacheName) {
+                        if (cacheName !== APP_CACHE_NAME && cacheName !== STATIC_CACHE_NAME) {
+                            console.log('deleting',cacheName);
+                            return caches.delete(cacheName);
+                        }
+                    })
+                );
+            })
+        ])
+    );
+});
 
-  self.addEventListener("fetch", function(e){
-    e.respondWith(
-       caches.match(e.request).then(function(response) {
-         return response || fetch(e.request);
-       })
-     )
+  this.addEventListener('fetch', function(event) {
+    var response;
+    event.respondWith(caches.match(event.request)
+      .then(function (match) {
+        return match || fetch(event.request);
+      }).catch(function() {
+        return fetch(event.request);
+      })
+      .then(function(r) {
+        response = r;
+        caches.open(cacheName).then(function(cache) {
+          cache.put(event.request, response);
+        });
+        return response.clone();
+      })
+    );
   });
