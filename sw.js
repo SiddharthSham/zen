a1bd0605f14981dd9c823d1c38e0c2dec49faedf
Siddharth Sham <siddhsham@gmail.com>
2018-08-20 20:54:06 +0530

    Workbox Service Worker implemented

--- sw.js
+++ sw.js
@@ -1,3 +1,52 @@
+<<<<<<< HEAD
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js');
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
+
+workbox.routing.registerRoute(
+  /\.(?:js|css)$/,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'static-resources',
+  }),
+);
+
+workbox.routing.registerRoute(
+  // Cache image files
+  /.*\.(?:png|jpg|jpeg|svg|gif)/,
+  // Use the cache if it's available
+  workbox.strategies.cacheFirst({
+    // Use a custom cache name
+    cacheName: 'image-cache',
+    plugins: [
+      new workbox.expiration.Plugin({
+        // Cache only 20 images
+        maxEntries: 20,
+        // Cache for a maximum of athree days
+        maxAgeSeconds: 3 * 24 * 60 * 60,
+      })
+    ],
+  })
+);
+
+workbox.routing.registerRoute(
+  new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'google-fonts',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 3,
+      }),
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200]
+      }),
+    ],
+  }),
+);
+=======
 ---
 layout: null
 ---
@@ -74,3 +123,4 @@ self.addEventListener('fetch', function(event) {
       })
     );
   });*/
+>>>>>>> 1080a923a87653b90376e95a5517f9528d24d82d
