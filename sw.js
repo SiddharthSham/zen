58673d044d8c245885872198a7e48cddac29d4b8
Siddharth Sham <siddhsham@gmail.com>
2018-08-20 15:32:04 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -29,7 +29,7 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('fetch', function(event) {
   var updateCache = function(request){
-    return caches.open('zen-app').then(function (cache) {
+    return caches.open(CACHE_NAME).then(function (cache) {
       return fetch(request).then(function (response) {
         console.log('add page to offline'+response.url)
         return cache.put(request, response);
